384de03fb091a4183e6b0c69f2fd54c5d319bc8f
Zachary Kuchar <zacharyk323@live.com>
2024-02-15 13:40:07 -0500

    Tab widget code

--- QT.py
+++ QT.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget, QLineEdit, QHBoxLayout, QVBoxLayout, QGridLayout, QLabel, QPushButton, QApplication, QSizePolicy, QMessageBox, QMainWindow, QToolBar,QPushButton,QStatusBar,QLabel
+from PySide6.QtWidgets import QWidget, QTabWidget, QLineEdit, QHBoxLayout, QVBoxLayout, QGridLayout, QLabel, QPushButton, QApplication, QSizePolicy, QMessageBox, QMainWindow, QToolBar,QPushButton,QStatusBar,QLabel
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap
 
@@ -83,6 +83,41 @@ grid.addWidget(button_7,2,2)
 grid.addWidget(button_8,0,4,3,1)
 grid.addWidget(button_9,3,2,1,3)
 grid.addWidget(button_10,3,0,1,1)
+
+#Tab widget copied code
+        # tab_widget = QTabWidget(self)
+
+        # #Information
+        # widget_form = QWidget()
+        # label_full_name = QLabel("Full name :")
+        # line_edit_full_name = QLineEdit()
+        # form_layout = QHBoxLayout()
+        # form_layout.addWidget(label_full_name)
+        # form_layout.addWidget(line_edit_full_name)
+        # widget_form.setLayout(form_layout)
+
+        # #Buttons
+        # widget_buttons = QWidget()
+        # button_1 = QPushButton("One")
+        # button_1.clicked.connect(self.button_1_clicked)
+        # button_2 = QPushButton("Two")
+        # button_3 = QPushButton("Three")
+        # buttons_layout = QVBoxLayout()
+        # buttons_layout.addWidget(button_1)
+        # buttons_layout.addWidget(button_2)
+        # buttons_layout.addWidget(button_3)
+        # widget_buttons.setLayout(buttons_layout)
+
+
+        # #Add tabs to widget
+        # tab_widget.addTab(widget_form,"Information")
+        # tab_widget.addTab(widget_buttons,"Button")
+
+
+        # layout = QVBoxLayout()
+        # layout.addWidget(tab_widget)
+
+        # self.setLayout(layout)
 
 layout.addLayout(layouttwo)
 layout.addLayout(grid)

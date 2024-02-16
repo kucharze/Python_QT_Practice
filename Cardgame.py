a64c3dec2ff71dfe8d59671b4d3bad38651925a4
Zachary Kuchar <zacharyk323@live.com>
2024-02-16 17:40:31 -0500

    Setup for a cardgame program

---
+++ Cardgame.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import QWidget, QTabWidget, QLineEdit, QHBoxLayout, QVBoxLayout, QGridLayout, QLabel, QPushButton, QApplication, QSizePolicy, QMessageBox, QMainWindow, QToolBar,QPushButton,QStatusBar,QLabel
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QPixmap
+
+app = QApplication()
+
+window = QMainWindow()
+
+app.exec()

991a53ed65a90ff5a7eb9456a2d3d59040a3c754
Zachary Kuchar <zacharyk323@live.com>
2024-02-16 17:42:35 -0500

    Initial program setup

--- Cardgame.py
+++ Cardgame.py
@@ -2,8 +2,20 @@ from PySide6.QtWidgets import QWidget, QTabWidget, QLineEdit, QHBoxLayout, QVBox
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap
 
+# def addPic():
+#     print("Add pic")
+#     Cardpic = QLabel("Hello There good sir how are you today?")
+#     Cardpic.setPixmap(QPixmap('./2c.png'))
+#     layout.addWidget(Cardpic)
+
 app = QApplication()
 
 window = QMainWindow()
+
+wid = QWidget()
+layout = QVBoxLayout()
+wid.setLayout(layout)
+
+window.setCentralWidget(wid)
 
 app.exec()
